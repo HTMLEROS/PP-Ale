d1fb3f98971e4cc4b0879c0a2ff529898206946d
htmleros <htmlerosavjk@gmail.com>
2025-10-31 11:22:43 -0300

    Delete Datos.py

--- Datos.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-st.image('images/banner.png')
-
-st.title("Aanálisis de Datos en la Facultad de Informática")
-st.subheader(" Con Python🐍")
-st.write("¿Qué sabemos de el dataset de Aeropuyertos Argentinos...")
