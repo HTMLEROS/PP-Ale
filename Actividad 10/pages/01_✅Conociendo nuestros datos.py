48dadf06b9cc3994953a52fb7305ce822e9af2c7
htmleros <htmlerosavjk@gmail.com>
2025-11-12 12:24:27 -0300

    Add files via upload

--- Actividad 10/pages/01_✅Conociendo nuestros datos.py
+++ Actividad 10/pages/01_✅Conociendo nuestros datos.py
@@ -3,10 +3,10 @@ import streamlit as st
 
 aeropuertos =  pd.read_csv('ar-airports.csv')
 st.title("Parte 1")
-st.header("Datos que encontraron")
+st.header("Datos que encontramos")
 st.write("Este Dataset (ar-airports.csv) contiene información sobre todos los puertos, aeropuertos, helipuertos y bases de despegue de globos aeroestáticos de la república.")
 st.write("Para empezar, trabajaremos con el tamaño del dataset en tablas y columnas, los nombres de los aeropuertos que aparecen y de que tamaño son.")
-st.write("POdemos observar que existen 6 tipos de aeropuertos: Grandes, Medianos, Chicos, Helipuertos, Ballonports y Cerrados. Tambien, observamos que sus columnas estan organizadas por ID, Nombre del aeropuerto, Ubicación entre otros datos relevantes.")
+st.write("Podemos observar que existen 6 tipos de aeropuertos: Grandes, Medianos, Chicos, Helipuertos, Ballonports y Cerrados. Tambien, observamos que sus columnas estan organizadas por ID, Nombre del aeropuerto, Ubicación entre otros datos relevantes.")
 filas, columnas = aeropuertos.shape
 with st.expander("¿Cuántas filas y columnas tiene el dataset?"):
     filas, columnas = aeropuertos.shape
